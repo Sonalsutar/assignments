cc289ab15ebe289c35e9ddcb80c068ad729141ab
Sonalsutar <sonalsutar8088@gmail.com>
2021-10-12 04:29:12 -0700

    Designing of pyramid

--- pyramid.py
+++ pyramid.py
@@ -1,3 +1,7 @@
-num=10
+num=int(input)
 for i in range (1,num+1):
-    print(" "*(num-i)+" * "*i)
+    print(" "*(num-i)+"* "*i)
+
+num=int(input("enter no. of rows:"))
+for i in range (1,num+1):
+    print(" "*(num-i)+"* "*i)
