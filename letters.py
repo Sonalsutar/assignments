7bedd2d4e9c27c070579384ca312fc52b622ea16
Sonalsutar <sonalsutar8088@gmail.com>
2021-11-08 20:53:46 -0800

    uppercase and lowercase letters

---
+++ letters.py
@@ -0,0 +1,7 @@
+import string
+print("\n Enter all the alphabet in lowercase :")
+for letter in string.ascii_uppercase:
+   print(letter, end =" ")
+print("\nEnter all the alphabets in uppercase:")
+for letter in string.ascii_lowercase:
+   print(letter, end =" ")
