53de07d53dd6eefa818b7dca281eeca25677db0f
Sonalsutar <sonalsutar8088@gmail.com>
2021-11-10 08:22:39 -0800

    string transalation

--- strtranslate.py
+++ strtranslate.py
@@ -2,7 +2,7 @@ def test(str):
 	return str.translate(str.maketrans('PTSHA', '90168'))
 str = "PHP"
 print(" string: \n",str)
-print("number: \n",test(str))
+print("translated string: \n",test(str))
 str = "JAVASCRIPT"
 print(" string: \n",str)
-print("number: \n",test(str))
+print("translated string: \n",test(str))
