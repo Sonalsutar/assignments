4da407cda0fb9d2d800e7038487c77e4bfdc010d
Sonalsutar <sonalsutar8088@gmail.com>
2021-11-11 21:17:52 -0800

    converting list into float numbers

---
+++ confloat.py
@@ -0,0 +1,6 @@
+my_list = ["1.1","1.2","1.3","1.4","1.5","1.6","1.7","1.8","1.9"]
+print(my_list)
+list_of_floats = []
+for item in my_list:
+    list_of_floats.append(float(item))
+print("the  float converted is:",list_of_floats)
