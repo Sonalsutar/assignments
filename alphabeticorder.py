f421e2d692678346dae6229b02a4c31be18226df
Sonalsutar <sonalsutar8088@gmail.com>
2021-11-11 21:17:14 -0800

    converting string into alphabetical order

---
+++ alphabeticorder.py
@@ -0,0 +1,14 @@
+def string(str1):
+    return ''.join(sorted(str1))
+
+str1 = "PHP"
+print("\n String1:",str1)
+print("After converting the string in alphabetical order :", string(str1))
+
+str1 = "javascript"
+print("\n String2:",str1)
+print("After converting the string in  string in alphabetical order:", string(str1))
+
+str1 = "python"
+print("\n String3:",str1)
+print("After converting the string in alphabetical order:", string(str1))
